838a2f341969148b3f149572cb92d340792bc6ab
Nekit1025 <nekit1025l@gmail.com>
2026-01-20 18:39:38 +0300

    fix: redis connect

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -15,6 +15,7 @@ dotenv.config();
         return new Redis({
           host: process.env.REDIS_HOST,
           port: Number(process.env.REDIS_PORT),
+          password: process.env.REDIS_PASSWORD,
         });
       },
     },
